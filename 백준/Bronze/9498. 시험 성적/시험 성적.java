e5cddfe649d6d8098e7d8a0ceb081f2b64fa9a94
imin94 <128824816+imin94@users.noreply.github.com>
2023-12-05 17:28:03 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		if(num>=90)
+			bw.write("A");
+		else if(num>=80)
+			bw.write("B");
+		else if(num>=70)
+			bw.write("C");
+		else if(num>=60)
+			bw.write("D");
+		else
+			bw.write("F");
+		
+		br.close();
+		bw.close();
+	}
+
+}
