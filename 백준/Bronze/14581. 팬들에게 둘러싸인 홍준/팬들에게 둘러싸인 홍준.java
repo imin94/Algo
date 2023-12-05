e13e4120fe2502fc9fb316d8fb062a015aeafe92
imin94 <128824816+imin94@users.noreply.github.com>
2023-12-05 17:41:29 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18244 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String hong = sc.next();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+hong+"::fan:");
+		System.out.print(":fan::fan::fan:");
+
+	}
+
+}
