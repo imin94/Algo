0eb84cf7f86b9c5aedd0ca579ddc49baf48dfdec
imin94 <128824816+imin94@users.noreply.github.com>
2023-12-05 17:30:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long ans = Long.parseLong(st.nextToken());
+		ans+=Long.parseLong(st.nextToken());
+		ans+=Long.parseLong(st.nextToken());
+		
+		bw.write(String.valueOf(ans));
+		br.close();
+		bw.close();
+		
+	}
+
+}
