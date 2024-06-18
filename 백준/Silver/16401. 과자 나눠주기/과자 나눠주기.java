c2e40d270d2e3d789765dce9364418ffd9de5d90
imin94 <128824816+imin94@users.noreply.github.com>
2024-06-18 21:19:10 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1704 ms, Memory: 147008 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int n;
+    public static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<m; i++)
+            list.add(Integer.parseInt(st.nextToken()));
+        Collections.sort(list);
+
+        int l = 1;
+        int r = list.get(m-1);
+        while(l<=r){
+            int mid = (l+r) >>> 1;
+            if(find(mid))
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        bw.write(r+"\n");
+        br.close();
+        bw.close();
+    }
+
+    public static boolean find(int k){
+        int cnt = 0;
+        for(int i = list.size()-1; i>=0; i--){
+            cnt += list.get(i)/k;
+            if(cnt>=n)
+                return true;
+        }
+        return false;
+    }
+}
